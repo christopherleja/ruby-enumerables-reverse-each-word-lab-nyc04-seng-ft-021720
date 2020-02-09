df39bd90130258120b3c6b862d512c7e2bae3ffd
Christopher Leja <christopher.leja@gmail.com>
2020-02-09 19:13:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,13 @@
 def reverse_each_word(array)
   words_split = array.split(" ")
-  result = words_split.each do | w | p w.reverse.join(" ")
+  result = words_split.each do | w | p w.reverse
 end
 result
 end
   
-
+def reverse_each_word_with_collect(array)
+  words_split = array.split(" ")
+  results = words_split.collect do | w | p w.reverse
+end
+results
+end
