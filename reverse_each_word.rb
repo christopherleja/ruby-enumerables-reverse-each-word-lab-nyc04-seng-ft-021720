d8110dbcb65f781e83cecb97c445ef5bd880c1df
Christopher Leja <christopher.leja@gmail.com>
2020-02-09 19:07:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(argument)
-  words_array = argument
-  words_reversed = argument.each do | w |
+ 
+  words_array = argument.each do | w |
    w.reverse
-   words_reversed
+   words_array
 end
-p words_reversed
+p words_array
 end
   
 
